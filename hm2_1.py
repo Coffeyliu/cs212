6a848d5346f47be0715faee6056df221ae462243
shirouto <28932673+shirouto@users.noreply.github.com>
2020-03-17 17:26:35 +0000

    hm2_1: 3/5

--- hm2_1.py
+++ hm2_1.py
@@ -22,28 +22,44 @@ def compile_formula(formula, verbose=False):
     is called with Y eqal to 0, the function should return False."""
     
     # modify the code in this function.
+    # FB: a `set` is a **not-ordered** collection. `list` is ordered
+    # and the docstring above is specifically stating that the order of the
+    # letters should be the same as in the original formula string. So this is
+    # a bug: your call to set may scramble the order of the letters. Not to mention
+    # that this is the source of your non-determinism you are groping with later.
     letters = ''.join(set(re.findall('[A-Z]', formula)))
 
     #NOTE:I use len() because I know in this game length of the formular should be small. 
+    # FB: why `ascii_letters`? You have only uppercase letters; apparently,
+    # based on the compile_word implementation, word.isupper is the only test needed to qualify as
+    # a numeric expression encoding (not operator), so why not do the same? that is already
+    # stricter than `c[0] in bag_of_letters` and faster ;-)
+    # FB: you called the **same** re.split twice, here and for tokens: it is a waste (regexp are faster than 
+    # iterating through the string in python, but they do take time, so...)
     first_letter = [c[0] for c in re.split('([A-Z]+)', formula) if len(c) > 1 and c[0] in string.ascii_letters]      
     parms = ', '.join(letters)
     tokens = map(compile_word, re.split('([A-Z]+)', formula))
     body = ''.join(tokens)
 
     if first_letter:
+        # FB: nice! you shortcut the body evaluation if a beginning letter is 0
         body = ' and '.join('%s != 0' % (c) for c in first_letter) + ' and ' + body 
 
     f = 'lambda %s: %s' % (parms, body)
+
     if verbose: print(f)
+
     return eval(f), letters
 
 def compile_word(word):
     """Compile a word of uppercase letters as numeric digits.
     E.g., compile_word('YOU') => '(1*U+10*O+100*Y)'
     Non-uppercase words uncahanged: compile_word('+') => '+'"""
+
     if word.isupper():
         terms = [('%s*%s' % (10**i, d)) 
                  for (i, d) in enumerate(word[::-1])]
+
         return '(' + '+'.join(terms) + ')'
     else:
         return word
@@ -53,10 +69,12 @@ def faster_solve(formula):
     Input formula is a string; output is a digit-filled-in string or None.
     This version precompiles the formula; only one eval per formula."""
     f, letters = compile_formula(formula)
+
     for digits in itertools.permutations((1,2,3,4,5,6,7,8,9,0), len(letters)):
         try:
             if f(*digits) is True:
                 table = str.maketrans(letters, ''.join(map(str, digits)))
+
                 return formula.translate(table)
         except ArithmeticError:
             pass
@@ -65,5 +83,6 @@ def test():
     assert faster_solve('A + B == BA') == None # should NOT return '1 + 0 == 01'
     assert faster_solve('YOU == ME**2') in ['289 == 17**2', '576 == 24**2', '841 == 29**2', '324 == 18**2']
     assert faster_solve('X / X == X') == '1 / 1 == 1'
+
     return 'tests pass'
 print(test())
