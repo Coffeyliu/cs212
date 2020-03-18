8f7c4192519884cf1693bd9b64cc9ef83c4c18a9
Coffeyliu <taliu@wiley.com>
2020-03-18 13:20:35 -0400

    modified code after comments

--- hm2_1.py
+++ hm2_1.py
@@ -14,6 +14,8 @@
 import re
 import itertools
 import string
+from collections import OrderedDict
+
 
 def compile_formula(formula, verbose=False):
     """Compile formula into a function. Also return letters found, as a str,
@@ -27,7 +29,9 @@ def compile_formula(formula, verbose=False):
     # letters should be the same as in the original formula string. So this is
     # a bug: your call to set may scramble the order of the letters. Not to mention
     # that this is the source of your non-determinism you are groping with later.
-    letters = ''.join(set(re.findall('[A-Z]', formula)))
+
+    # letters = ''.join(set(re.findall('[A-Z]', formula)))    # old approach
+    letters = ''.join(list(OrderedDict((element, None) for element in re.findall('[A-Z]', formula)))) #new approach
 
     #NOTE:I use len() because I know in this game length of the formular should be small. 
     # FB: why `ascii_letters`? You have only uppercase letters; apparently,
@@ -36,9 +40,12 @@ def compile_formula(formula, verbose=False):
     # stricter than `c[0] in bag_of_letters` and faster ;-)
     # FB: you called the **same** re.split twice, here and for tokens: it is a waste (regexp are faster than 
     # iterating through the string in python, but they do take time, so...)
-    first_letter = [c[0] for c in re.split('([A-Z]+)', formula) if len(c) > 1 and c[0] in string.ascii_letters]      
+
+    # first_letter = [c[0] for c in re.split('([A-Z]+)', formula) if len(c) > 1 and c[0] in string.ascii_letters] # old approach
+    temp =  re.split('([A-Z]+)', formula)
+    first_letter = [c[0] for c in temp if len(c) > 1 and c[0].isupper()]    # new approach     
     parms = ', '.join(letters)
-    tokens = map(compile_word, re.split('([A-Z]+)', formula))
+    tokens = map(compile_word, temp)
     body = ''.join(tokens)
 
     if first_letter:
@@ -46,9 +53,7 @@ def compile_formula(formula, verbose=False):
         body = ' and '.join('%s != 0' % (c) for c in first_letter) + ' and ' + body 
 
     f = 'lambda %s: %s' % (parms, body)
-
     if verbose: print(f)
-
     return eval(f), letters
 
 def compile_word(word):
